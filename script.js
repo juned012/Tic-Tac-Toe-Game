529fb25427a6e3b2d33a59df38f6ecaec94fe078
juned072 <junedansari9172@gmail.com>
2024-12-26 16:51:37 +0530

    Added a winning pattern

--- script.js
+++ script.js
@@ -1,13 +1,56 @@
 let boxes = document.querySelectorAll(".box");
 let currentPlayer = "X";
+let isGameStart = true;
+const winnerPlayer = document.getElementById("winner");
+
+const winningPatterns = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+
+function checkWinning() {
+  for (let pattern of winningPatterns) {
+    const [a, b, c] = pattern;
+    if (
+      boxes[a].innerHTML === currentPlayer &&
+      boxes[b].innerHTML === currentPlayer &&
+      boxes[c].innerHTML === currentPlayer
+    ) {
+      winnerPlayer.innerText = `Player ${currentPlayer} is Wins🎉✨!`;
+      isGameStart = false;
+      return true;
+    }
+  }
+  return false;
+}
+
+function checkDraw() {
+  const isDraw = Array.from(boxes).every((box) => box.innerHTML !== "");
+  if (isDraw && isGameStart) {
+    winnerPlayer.innerText = "It's a Draw!";
+    isGameStart = false;
+  }
+}
 
 function renderBoard() {
   boxes.forEach((box) => {
     box.addEventListener("click", () => {
-      if (box.innerHTML === "") {
+      if (isGameStart && box.innerHTML === "") {
         box.innerHTML = currentPlayer;
-        currentPlayer = currentPlayer === "X" ? "O" : "X";
-        updateCurrentPlayer();
+        const hasWinner = checkWinning();
+        if (!hasWinner) {
+          checkDraw();
+          if (isGameStart) {
+            currentPlayer = currentPlayer === "X" ? "O" : "X";
+            updateCurrentPlayer();
+          }
+        }
       }
     });
   });
@@ -15,8 +58,21 @@ function renderBoard() {
 
 function updateCurrentPlayer() {
   const currentPlayers = document.getElementById("currentPlayer");
-  currentPlayers.innerText = `${currentPlayer}`;
+  currentPlayers.innerText = currentPlayer;
 }
+
+function restartGame() {
+  currentPlayer = "X";
+  isGameStart = true;
+  boxes.forEach((box) => {
+    box.innerHTML = "";
+  });
+  winnerPlayer.innerText = "";
+  updateCurrentPlayer();
+}
+
+const restart = document.getElementById("restartGame");
+restart.addEventListener("click", restartGame);
 
 renderBoard();
 updateCurrentPlayer();
